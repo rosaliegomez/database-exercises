aef11ef9b3ff7729123250489cd9db14a3b26b11
rosaliework <rosalie.gomez12@gmail.com>
2017-10-11 09:52:57 -0500

    add content to albums db

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
-
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
 id  INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist  VARCHAR(100)  NOT NULL,
-name  VARCHAR (100) NOT NULL,
-release_date DATE NOT NULL,
+artist  VARCHAR(200)  NOT NULL,
+name  VARCHAR (200) NOT NULL,
+release_date YEAR(4) NOT NULL,
 sales DECIMAL(10,2) NOT NULL,
 genre VARCHAR (50)  NOT NULL,
 PRIMARY KEY (id)
